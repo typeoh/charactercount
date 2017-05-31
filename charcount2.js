4ffea9f663ce2437bbcd39030c94ce9c779afb47
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-05-31 21:52:09 +0000

    Created new file that returns indices.

--- charcount2.js
+++ charcount2.js
@@ -1,15 +1,5 @@
-// function countLengths(strings) {
-//   return strings.reduce(function(output, string) {
-//     // This is a "guard statement" that protects line 71 below from situations where the key is undefined
-//     if (!output[string.length]) {
-//       output[string.length] = 0;
-//     }
-//     output[string.length] += 1;
-//     return output;
-//   }, {});
-// }
 function countLengths(str){
-  //First we make the string lowercase, take out the spaces, and split each character into an array of strings
+  //First we make the string lowercase, take out the spaces
   var noSpaces = str.split(" ").join("").toLowerCase();
   //empty variable that we will push the result of the .foreach to
   var chars = {};
@@ -17,15 +7,18 @@ function countLengths(str){
   for (var i = 0; i < noSpaces.length; i++) {
     // letter variable is the character at whatever index it is on the count
      var letter = noSpaces[i];
+
      // if .foreach returns undefined (means there is only one of these characters) letter count is 1
     if(chars[letter] === undefined){
-      chars[letter] = 1
+      //now chars letter is return the index number
+      chars[letter] = [i];
       //if .forEach returns <=1 then we add a value of 1 to the amount of letter found
     } else {
-      chars[letter] = chars[letter] +  1;
+      chars[letter].push(i);
     }
+     // var index = str.indexOf(chars[letter]);
   }
   return chars;
 }
 
-console.log(countLengths("l"));
+console.log(countLengths("batty"));
